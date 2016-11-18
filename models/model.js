0bc77cc3586611411eed5e63c1da67bef286b6f0
Gustavo Abbott <GCamposAbbott@gmail.com>
2016-11-18 01:54:40 -0500

    average color get and put working

--- models/model.js
+++ models/model.js
@@ -3,19 +3,14 @@ var Schema = mongoose.Schema;
 
 // See http://mongoosejs.com/docs/schematypes.html
 
-var animalSchema = new Schema({
-	name: String,
-	// name: {type: String, required: true}, // this version requires this field to exist
-	// name: {type: String, unique: true}, // this version requires this field to be unique in the db
-	age: Number,
-	tags: [String],
-	description: {
-		weight: Number,
-		color: String
-	},
-	url: String,
+var averageColorSchema = new Schema({
+	city: String,
+	red: Number,
+	green: Number,
+	blue: Number,
+	localtime: String,
 	dateAdded : { type: Date, default: Date.now },
 })
 
 // export 'Animal' model so we can interact with it in other files
-module.exports = mongoose.model('Animal',animalSchema);
+module.exports = mongoose.model('AverageColor',averageColorSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 // our db model
-var Animal = require("../models/model.js");
-
-// simple route to render am HTML form that can POST data to our server
-// NOTE that this is not a standard API route, and is really for testing
-router.get('/create-pet', function(req,res){
-  res.render('pet-form.html')
-})
-
-// simple route to render an HTML page that pulls data from our server and displays it on a page
-// NOTE that this is not a standard API route, and is really for testing
-router.get('/show-pets', function(req,res){
-  res.render('show-pets.html')
-})
+var AverageColor = require("../models/model.js");
 
 /**
  * GET '/'
@@ -26,18 +14,22 @@ router.get('/show-pets', function(req,res){
 router.get('/', function(req, res) {
   
   var jsonData = {
-  	'name': 'node-express-api-boilerplate',
+  	'city': 'node-express-api-boilerplate',
   	'api-status':'OK'
   }
-
   // respond with json data
   res.json(jsonData)
 });
+
+
+
 
 // simple route to show an HTML page
 router.get('/sample-page', function(req,res){
   res.render('sample.html')
 })
+
+
 
 // /**
 //  * POST '/api/create'
@@ -47,49 +39,43 @@ router.get('/sample-page', function(req,res){
 //  */
 
 router.post('/api/create', function(req, res){
-
     console.log(req.body);
-
     // pull out the information from the req.body
-    var name = req.body.name;
-    var age = req.body.age;
-    var tags = req.body.tags.split(","); // split string into array
-    var weight = req.body.weight;
-    var color = req.body.color;
-    var url = req.body.url;
+    var city = req.body.city;
+    var red = req.body.red;
+    var green = req.body.green;
+    var blue = req.body.blue;
+    var localtime = req.body.localtime;
 
     // hold all this data in an object
     // this object should be structured the same way as your db model
-    var animalObj = {
-      name: name,
-      age: age,
-      tags: tags,
-      description: {
-        weight: weight,
-        color: color
-      },
-      url: url
+    var averagecolorObj = {
+      city: city,
+      red: red,
+      green: green,
+      blue: blue,
+      localtime: localtime
     };
 
     // create a new animal model instance, passing in the object
-    var animal = new Animal(animalObj);
+    var averagecolor = new AverageColor(averagecolorObj);
 
     // now, save that animal instance to the database
     // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save    
-    animal.save(function(err,data){
+    averagecolor.save(function(err,data){
       // if err saving, respond back with error
       if (err){
-        var error = {status:'ERROR', message: 'Error saving animal'};
+        var error = {status:'ERROR', message: 'Error saving average color'};
         return res.json(error);
       }
 
-      console.log('saved a new animal!');
+      console.log('saved a new average color!');
       console.log(data);
 
       // now return the json data of the new animal
       var jsonData = {
         status: 'OK',
-        animal: data
+        averagecolor: data
       }
 
       return res.json(jsonData);
@@ -109,18 +95,18 @@ router.get('/api/get/:id', function(req, res){
   var requestedId = req.params.id;
 
   // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
-  Animal.findById(requestedId, function(err,data){
+  AverageColor.findById(requestedId, function(err,data){
 
     // if err or no user found, respond with error 
     if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal'};
+      var error = {status:'ERROR', message: 'Could not find that avergae color'};
        return res.json(error);
     }
 
     // otherwise respond with JSON data of the animal
     var jsonData = {
       status: 'OK',
-      animal: data
+      averagecolor: data
     }
 
     return res.json(jsonData);
@@ -137,10 +123,10 @@ router.get('/api/get/:id', function(req, res){
 router.get('/api/get', function(req, res){
 
   // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
-  Animal.find(function(err, data){
+  AverageColor.find(function(err, data){
     // if err or no animals found, respond with error 
     if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find animals'};
+      var error = {status:'ERROR', message: 'Could not find average color'};
       return res.json(error);
     }
 
@@ -148,7 +134,7 @@ router.get('/api/get', function(req, res){
 
     var jsonData = {
       status: 'OK',
-      animals: data
+      averagecolor: data
     } 
 
     res.json(jsonData);
@@ -169,7 +155,7 @@ router.get('/api/search', function(req,res){
   console.log("we are searching for " + searchTerm);
 
   // let's find that animal
-  Animal.find({name: searchTerm}, function(err,data){
+  AverageColor.find({name: searchTerm}, function(err,data){
     // if err, respond with error 
     if(err){
       var error = {status:'ERROR', message: 'Something went wrong'};
@@ -186,7 +172,7 @@ router.get('/api/search', function(req,res){
 
     var jsonData = {
       status: 'OK',
-      animals: data
+      averagecolor: data
     } 
 
     res.json(jsonData);        
@@ -194,115 +180,6 @@ router.get('/api/search', function(req,res){
 
 })
 
-// /**
-//  * POST '/api/update/:id'
-//  * Receives a POST request with data of the animal to update, updates db, responds back
-//  * @param  {String} req.params.id - The animalId to update
-//  * @param  {Object} req. An object containing the different attributes of the Animal
-//  * @return {Object} JSON
-//  */
 
-router.post('/api/update/:id', function(req, res){
-
-   var requestedId = req.params.id;
-
-   var dataToUpdate = {}; // a blank object of data to update
-
-    // pull out the information from the req.body and add it to the object to update
-    var name, age, weight, color, url; 
-
-    // we only want to update any field if it actually is contained within the req.body
-    // otherwise, leave it alone.
-    if(req.body.name) {
-      name = req.body.name;
-      // add to object that holds updated data
-      dataToUpdate['name'] = name;
-    }
-    if(req.body.age) {
-      age = req.body.age;
-      // add to object that holds updated data
-      dataToUpdate['age'] = age;
-    }
-    if(req.body.weight) {
-      weight = req.body.weight;
-      // add to object that holds updated data
-      dataToUpdate['description'] = {};
-      dataToUpdate['description']['weight'] = weight;
-    }
-    if(req.body.color) {
-      color = req.body.color;
-      // add to object that holds updated data
-      if(!dataToUpdate['description']) dataToUpdate['description'] = {};
-      dataToUpdate['description']['color'] = color;
-    }
-    if(req.body.url) {
-      url = req.body.url;
-      // add to object that holds updated data
-      dataToUpdate['url'] = url;
-    }
-
-    var tags = []; // blank array to hold tags
-    if(req.body.tags){
-      tags = req.body.tags.split(","); // split string into array
-      // add to object that holds updated data
-      dataToUpdate['tags'] = tags;
-    }
-
-
-    console.log('the data to update is ' + JSON.stringify(dataToUpdate));
-
-    // now, update that animal
-    // mongoose method findByIdAndUpdate, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
-    Animal.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
-      // if err saving, respond back with error
-      if (err){
-        var error = {status:'ERROR', message: 'Error updating animal'};
-        return res.json(error);
-      }
-
-      console.log('updated the animal!');
-      console.log(data);
-
-      // now return the json data of the new person
-      var jsonData = {
-        status: 'OK',
-        animal: data
-      }
-
-      return res.json(jsonData);
-
-    })
-
-})
-
-/**
- * GET '/api/delete/:id'
- * Receives a GET request specifying the animal to delete
- * @param  {String} req.params.id - The animalId
- * @return {Object} JSON
- */
-
-router.get('/api/delete/:id', function(req, res){
-
-  var requestedId = req.params.id;
-
-  // Mongoose method to remove, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
-  Animal.findByIdAndRemove(requestedId,function(err, data){
-    if(err || data == null){
-      var error = {status:'ERROR', message: 'Could not find that animal to delete'};
-      return res.json(error);
-    }
-
-    // otherwise, respond back with success
-    var jsonData = {
-      status: 'OK',
-      message: 'Successfully deleted id ' + requestedId
-    }
-
-    res.json(jsonData);
-
-  })
-
-})
 
 module.exports = router;
